8ab74d3882bdb132ed24d216f1fe6f8ea7776542
Aliyan Momin <aliyanmodi2003@gmail.com>
2025-03-09 20:24:43 -0500

    Removed deprecated MongoDB options

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 require("dotenv").config();
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
 
 const app = express();
 const port = process.env.PORT || 5001;
 
-// Connect to DB
+// Connect to MongoDB Atlas
 connectDB();
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('POS System Backend is Running...');
+app.get("/", (req, res) => {
+    res.send("POS System Backend is Running...");
 });
 
 app.listen(port, () => {
